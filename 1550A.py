de871925d9468fefdc6673f15c16ab27130d4fa5
bijon <bijonndc@gmail.com>
2021-07-16 11:36:05 +0600

    Find the array.py

---
+++ 1550A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 21:57:55 2021
+
+@author: Admin
+"""
+
+import math
+t = int(input())
+for _ in range(t) :
+    s = int(input())
+    print(math.ceil(math.sqrt(s)))
+
