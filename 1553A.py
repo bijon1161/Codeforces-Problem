6e18bd17b8ac0575bfd902584a3fba3e3d257d55
bijon <bijonndc@gmail.com>
2021-07-23 20:06:02 +0600

    Digits sum.py

--- 1553A.py
+++ 1553A.py
@@ -5,18 +5,9 @@ Created on Fri Jul 23 19:10:06 2021
 @author: Admin
 """
 
-for _ in range(int(input())):
-    p=input()
-    s=list(map(int,p))
-    if s[0]<9 and len(s)==1:
-        print("0")
-    elif s[0]==9 and len(s)==1:
-        print("1")
-    elif s[-1]==9 and len(s)>1:
-        print((int(p)//10)+1)
-    elif s[-1]!=9 and len(s)>1:
-        print(int(p)//10)
-    else:
-        print("0")
+t=int(input())
+for i in range(t):
+    n=int(input())
+    print((n+1)//10)
     
     
